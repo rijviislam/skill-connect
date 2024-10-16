77ed245268bf4f8c267840defff4d7ef2f33edc9
dishamallik <dishamallik758@gmail.com>
2024-10-16 15:00:12 +0600

    Feat: added cleint profle route and freelancer prtofile fix

---
+++ src/app/api/clientProfile/route.js
@@ -0,0 +1,15 @@
+import connectDB from '@/lib/connectDB';
+
+export const GET = async (request) => {
+    try {
+        const db = await connectDB();
+        const userCollection = db.collection("users");
+
+        
+        const freelancers = await userCollection.find({ role: "client" }).toArray();
+
+        return Response.json(freelancers);
+    } catch (error) {
+        return Response.json({ message: "Something went wrong" }, { status: 500 });
+    }
+}
---
+++ src/app/clientProfile/page.jsx
@@ -0,0 +1,8 @@
+
+import React from 'react'
+import ClientProfile from '../components/ClientProfile'
+
+export default function page() {
+  return <ClientProfile/>
+  
+}
