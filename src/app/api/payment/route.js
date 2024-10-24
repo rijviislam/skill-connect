52984ec4f4a2fbed1fc31a929e8ef553f9b65c56
rijviislam <rijviislam54@gmail.com>
2024-10-24 06:23:10 +0600

    refactor: refactored code

--- src/app/api/payment/route.js
+++ src/app/api/payment/route.js
@@ -1,8 +1,14 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 export async function POST(request) {
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+  if (!process.env.STRIPE_SECRET_KEY) {
+    return res.status(500).json({ error: "Stripe secret key is missing" });
+  }
+
   try {
     const { amount } = await request.json();
     const paymentIntent = await stripe.paymentIntents.create({
