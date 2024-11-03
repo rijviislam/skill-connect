9ba8d19f25dda4135a5f0cf28e7fb2f7d7497421
rijviislam <rijviislam54@gmail.com>
2024-11-03 18:34:02 +0600

    fix: fix the protected routes

--- src/middleware.js
+++ src/middleware.js
@@ -1,38 +1,18 @@
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 
-async function fetchUserRole(authToken) {
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_NEXT_URL}/api/get-user-role`, 
-      {
-        headers: { Authorization: `Bearer ${authToken}` },
-      }
-    );
-
-    if (!response.ok) {
-      console.error("Failed to fetch user role, status:", response.status);
-      throw new Error("Failed to fetch user role");
-    }
-
-    const data = await response.json();
-    console.log("User role fetched:", data.role);
-    return data.role;
-  } catch (error) {
-    console.error("Error in fetchUserRole:", error);
-    return null;
-  }
-}
-
-export async function middleware(request) {
+export async function middleware(req) {
   console.log("Middleware is Running");
-  
 
-  const authToken = request.cookies.get("next-auth.session-token")?.value;
-  const pathname = request.nextUrl.pathname;
+  const authToken = await getToken({ req, secret: process.env.NEXT_PUBLIC_AUTH_SECRET });
+  const pathname = req.nextUrl.pathname;
+
   console.log("Middleware is Running in Production");
   console.log("Auth Token in Production:", authToken);
 
-  console.log("Auth Token:", authToken);
+  // Check if authToken exists and directly use role if available
+  const userRole = authToken?.role || null;
+  console.log("User Role from Token:", userRole);
 
   const clientOnlyRoutes = [
     "/dashboard/job",
@@ -54,7 +34,7 @@ export async function middleware(request) {
 
   // Redirect authenticated users trying to access auth pages
   if (authToken && isAuthPage) {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/", req.url));
   }
 
   // Allow unauthenticated users to access auth pages
@@ -62,12 +42,7 @@ export async function middleware(request) {
     return NextResponse.next();
   }
 
-  // Fetch user role if authenticated
-  const userRole = authToken ? await fetchUserRole(authToken) : null;
-
-  console.log("User Role:", userRole);
-
-  // Role-based access control
+  // Role-based access control based on token's role
   if (
     (userRole === "client" && clientOnlyRoutes.includes(pathname)) ||
     (userRole === "freelancer" && freelancerOnlyRoutes.includes(pathname)) ||
@@ -78,7 +53,7 @@ export async function middleware(request) {
 
   // If user role does not match or is null, redirect to home
   console.log("Redirecting to home due to unauthorized access");
-  return NextResponse.redirect(new URL("/", request.url));
+  return NextResponse.redirect(new URL("/", req.url));
 }
 
 export const config = {
@@ -96,3 +71,4 @@ export const config = {
     "/dashboard/report",
   ],
 };
+
