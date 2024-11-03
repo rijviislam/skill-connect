3cd1a1e6b0536df0efc7e7e17d918154907a9e9a
dishamallik <dishamallik758@gmail.com>
2024-11-03 22:04:44 +0600

    update profile

--- src/app/components/(home)/WorkflowSteps.jsx
+++ src/app/components/(home)/WorkflowSteps.jsx
@@ -12,28 +12,28 @@ const WorkflowSteps = () => {
 
   return (
     <div className="py-12">
-      <div className="container mx-auto px-6">
-        <h2 className="text-left text-4xl font-medium text-[#ba6ae8] mb-7 mt-8 flex items-center">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <h2 className="text-left text-3xl sm:text-4xl font-medium text-[#ba6ae8] mb-6 sm:mb-8 mt-8 flex items-center">
           How It Works
           <FaArrowRight className="ml-2" />
         </h2>
         
-        <div className="flex flex-col md:flex-row justify-center items-center space-y-8 md:space-y-0 md:space-x-8">
+        <div className="flex flex-col md:flex-row flex-wrap justify-center items-center gap-8">
           
           {/* Step 1 */}
           <div 
             data-aos="zoom-in-up" 
-            className="flex flex-col items-center text-center p-4 w-60 h-56 border-2 hover:border-violet-700 bg-violet-50 shadow-md rounded-2xl transform transition-transform hover:scale-105 hover:bg-violet-50 hover:shadow-xl">
+            className="flex flex-col items-center text-center p-4 w-64 h-64 border-2 hover:border-violet-700 bg-violet-50 shadow-md rounded-2xl transform transition-transform hover:scale-105 hover:bg-violet-50 hover:shadow-xl">
             <div className="w-16 h-16 flex items-center justify-center bg-[#ba6ae8] text-white mb-3 rounded-full transition-colors hover:bg-violet-600">
               <FaBriefcase className="text-3xl" />
             </div>
             <h3 className="text-lg font-semibold mb-1">Post a Job</h3>
-            <p className="text-gray-600 p-2">Clients post job listings with details of the project they need help with best needs.</p>
+            <p className="text-gray-600 p-2">Clients post job listings with details of the project they need help with.</p>
           </div>
           
           {/* Step 2 */}
           <div 
-            className="flex flex-col items-center text-center p-4 w-60 h-56 border-2 hover:border-violet-700 bg-violet-50 shadow-md rounded-2xl transform transition-transform hover:scale-105 hover:bg-violet-50 hover:shadow-xl" 
+            className="flex flex-col items-center text-center p-4 w-64 h-64 border-2 hover:border-violet-700 bg-violet-50 shadow-md rounded-2xl transform transition-transform hover:scale-105 hover:bg-violet-50 hover:shadow-xl" 
             data-aos="fade-up" 
             data-aos-delay="100"
             >
@@ -46,7 +46,7 @@ const WorkflowSteps = () => {
 
           {/* Step 3 */}
           <div 
-            className="flex flex-col items-center text-center p-4 w-60 h-56 border-2 hover:border-violet-700 bg-violet-50 shadow-md rounded-2xl transform transition-transform hover:scale-105 hover:bg-violet-50 hover:shadow-xl" 
+            className="flex flex-col items-center text-center p-4 w-64 h-64 border-2 hover:border-violet-700 bg-violet-50 shadow-md rounded-2xl transform transition-transform hover:scale-105 hover:bg-violet-50 hover:shadow-xl" 
             data-aos="fade-up" 
             data-aos-delay="200"
             >
@@ -59,7 +59,7 @@ const WorkflowSteps = () => {
           
           {/* Step 4 */}
           <div 
-            className="flex flex-col items-center text-center p-4 w-60 h-56 border-2 hover:border-violet-700 bg-violet-50 shadow-md rounded-2xl transform transition-transform hover:scale-105 hover:bg-violet-50 hover:shadow-xl" 
+            className="flex flex-col items-center text-center p-4 w-64 h-64 border-2 hover:border-violet-700 bg-violet-50 shadow-md rounded-2xl transform transition-transform hover:scale-105 hover:bg-violet-50 hover:shadow-xl" 
             data-aos="fade-up" 
             data-aos-delay="300"
             >
